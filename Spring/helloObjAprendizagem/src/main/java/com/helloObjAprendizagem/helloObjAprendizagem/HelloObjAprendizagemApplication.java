fba7c59148e75ab7e9a6bb08d673139e0d6d1e05
Millybraga <emilly.braga02@hotmail.com>
2021-04-26 17:48:37 -0300

    primeiras atividades de Spring

---
+++ Spring/helloObjAprendizagem/src/main/java/com/helloObjAprendizagem/helloObjAprendizagem/HelloObjAprendizagemApplication.java
@@ -0,0 +1,13 @@
+package com.helloObjAprendizagem.helloObjAprendizagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloObjAprendizagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloObjAprendizagemApplication.class, args);
+	}
+
+}
---
+++ Spring/helloObjAprendizagem/src/test/java/com/helloObjAprendizagem/helloObjAprendizagem/HelloObjAprendizagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloObjAprendizagem.helloObjAprendizagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloObjAprendizagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
